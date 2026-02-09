948ae623f7ee159b703fde92f31830274334e42d
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-02-09 23:53:14 +0000

    strings variables

---
+++ strings/1variables.cs
@@ -0,0 +1,24 @@
+string aFriend = "Michael";
+Console.WriteLine(aFriend);
+
+aFriend = "Toni";
+Console.WriteLine("Hello " + aFriend);
+
+
+// string interpolation
+
+Console.WriteLine($"Hello {aFriend}");
+
+
+// work with strings
+
+string firstFriend = "John";
+string secondFriend = "Maria";
+
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}.");
+
+
+// string length
+
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters. ");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters. ");
