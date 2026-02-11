b9d73d6ed6b7e88d9052c89d7d7e21d5471b95ca
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-02-11 16:00:07 +0000

    game changes

--- GN/main.cs
+++ GN/main.cs
@@ -2,47 +2,62 @@ using System;
 
 class Program
 {
+    static Random rnd = new Random();
     static void Main()
     {
         int numero = GerarNumero();
         
-        Console.WriteLine(numero); // Debug
         Console.WriteLine("Adivinha o número de 1-50: ");
 
-        string input = Console.ReadLine();
-        int guess = int.Parse(input);
-
         int tentativas = 3;
+        int guess = pedir_palpite();
 
         while (guess != numero && tentativas > 1)
         {
+            if (guess > numero)
+            {
+                Console.WriteLine("Mais Baixo!");
+            }
+            else
+            {
+                Console.WriteLine("Mais Alto!");
+            }
+
             tentativas --;
             Console.WriteLine($"Tentativas restantes: {tentativas}");
+
             guess = pedir_palpite();
         }
 
         if (guess == numero)
         {
-            Console.WriteLine("Parabéns! Adivinhaste o número");
+            Console.WriteLine("Parabéns, adivinhaste o número!");
         }
         else
         {
-            Console.WriteLine($"Acabaram as tentativas o número era {numero}");
+            Console.WriteLine($"Acabaram as tentativas o número era: {numero}");
         }
 
     }
 
     static int pedir_palpite()
     {
-        Console.WriteLine("Tenta outra vez: ");
-        string input = Console.ReadLine();
-        int guess = int.Parse(input);
-        return guess;
+        while(true)
+        {
+            Console.WriteLine("Tenta outra vez: ");
+            string? input = Console.ReadLine();
+            
+            if (int.TryParse(input, out int guess) && guess >= 1 && guess <= 50)
+            {
+                return guess;
+            }
+
+            Console.WriteLine("Input inválido! Escreve um número entre 1 e 50.");
+        }
     }
 
     static int GerarNumero()
     {
-        Random rnd = new Random();
         return rnd.Next(1, 51);
     }
 }
